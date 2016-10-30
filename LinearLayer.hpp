3ab799ce912520124c5f37a55501b67b650ca948
Og192 <og192liu@gmail.com>
2016-10-30 18:43:34 +0800

    back prob not been tested

--- LinearLayer.hpp
+++ LinearLayer.hpp
@@ -1,9 +1,7 @@
-#include <vector>
-#include <cmath>
-#include <random>
-
 #include "lznn_types.h"
 #include "lznn_math.hpp"
+#include <cmath>
+#include <random>
 
 class LinearLayer
 {
@@ -11,6 +9,8 @@ class LinearLayer
 
         Matrix W;
         Vector W0;
+        Vector *NextLinearWeightDelta;
+        Vector WeightDelta;
 
         LinearLayer(size_t dataSize, size_t inputSize, size_t outputSize)
         :LinearLayer(Matrix(dataSize, Vector(inputSize, 0.0)), dataSize, inputSize, outputSize)
@@ -19,7 +19,9 @@ class LinearLayer
         LinearLayer(Matrix input, size_t dataSize, size_t inputSize, size_t outputSize)
         :
             input(input),
-            output(Matrix(dataSize, Vector(outputSize, 0.0))), 
+            output(Matrix(dataSize, Vector(outputSize, 0.0))),
+            outputNotActiveted(Matrix(dataSize, Vector(outputSize, 0.0))),
+            WeightDelta(Vector(outputSize, 0.0)),
             W0(Vector(outputSize, 0.0)),
             W(Matrix(outputSize, Vector(inputSize, 0.0)))
         {
@@ -41,14 +43,27 @@ class LinearLayer
                     {
                         sum += W[i][j] * input[k][j];
                     }
-                    output[k][i] = avtivition(sum + W0[i]);
+                    outputNotActiveted[k][i] = sum + W0[i];
+                    output[k][i] = sigmoid(sum + W0[i]);
                 }
             }
         }
 
-        void BackPropagate()
+        void BackPropagate(double learningRate)
         {
             //TODO: back propagate
+            
+            for (int i = 0; i < outputSize; i++)
+            {
+                for (int j = 0; j < inputSize; j++)
+                {
+                    W[i][j] += learningRate * gredientW[i][j];
+                }
+            }
+            for (int i = 0; i < outputSize; i++)
+            {
+                W0[i] += learningRate * gredientW0[i];
+            }
         }
 
         Matrix *Output()
@@ -64,14 +79,21 @@ class LinearLayer
 
         Matrix input;
         Matrix output;
-        Matrix delta;
+        Matrix outputNotActiveted;
+        Matrix gredientW;
+        Vector gredientW0;
         size_t dataSize;
         size_t inputSize;
         size_t outputSize;
 
-        double avtivition(double in)
+        double sigmoid(double in)
         {
             double result = 1.0 / (1.0 + exp(-in));
+            return result;
+        }
+        double sigmoidGredient(double in)
+        {
+            double result = sigmoid(in) * (1 - sigmoid(in));
             return result;
         }
         void initW()
@@ -85,6 +107,35 @@ class LinearLayer
                     W[i][j] = distribution(generator);
                 }
                 W0[i] = distribution(generator);
+            }
+        }
+        void calculateGredientW()
+        {
+            Vector deltas(outputSize + 1, 0.0);
+            for(size_t i = 0; i < dataSize; i++)
+            {
+                for(size_t j = 0; j < outputSize; j++)
+                {
+                    double delta = sigmoidGredient(outputNotActiveted[i][j]) * (*NextLinearWeightDelta)[j];
+                    deltas[j] = delta;
+                }
+                for(size_t j = 0; j < outputSize; j++)
+                {
+                    for (size_t k = 0; k < inputSize; k++)
+                    {
+                        this->gredientW[j][k] += deltas[j] * input[i][k];
+                    }
+                    this->gredientW0[j] += deltas[j];
+                }
+            }
+            for (size_t j = 0; j < inputSize; j++)
+            {
+                double sum = 0;
+                for (size_t i = 0; i < outputSize; i++)
+                {
+                    sum += deltas[i] * this->W[j][i];
+                }
+                this->WeightDelta[j] = sum;
             }
         }
 };
---
+++ MLP.hpp
@@ -0,0 +1,11 @@
+#include "LinearLayer.hpp"
+#include "lznn_types.h"
+#include <list>
+
+class MLP
+{
+    public:
+        void 
+    private:
+        list<LinearLayer> mlp;
+};
